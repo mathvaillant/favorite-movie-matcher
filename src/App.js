9ba312472d1a3aecee71753f977072e4354c55e7
mathvaillant <matheusvaillant505@gmail.com>
2021-07-10 20:38:06 +0100

    Performance improvements

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-/* eslint-disable react/react-in-jsx-scope */
-
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './screens/Home'
 import Options from './components/Options/Options'
-
+import Spinner from './components/Spinner/Spinner'
 import './App.scss'
-import Likes from './screens/Likes'
-import Dislikes from './screens/Dislikes'
+
+const Home = React.lazy(() => import('./screens/Home'))
+const Likes = React.lazy(() => import('./screens/Likes'))
+const Dislikes = React.lazy(() => import('./screens/Dislikes'))
 
 function App() {
   return (
     <div className='App'>
       <Router>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/likes' component={Likes} />
-          <Route path='/dislikes' component={Dislikes} />
+          <Suspense fallback={<Spinner />}>
+            <Route exact path='/' component={Home} />
+            <Route path='/likes' component={Likes} />
+            <Route path='/dislikes' component={Dislikes} />
+          </Suspense>
         </Switch>
         <Options />
       </Router>
